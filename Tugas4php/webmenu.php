d37a5986c47cefc546035398ac6646045d2ee64b
Maulana Nofa Karisma <55683754+Maulananofa@users.noreply.github.com>
2024-04-02 01:52:51 +0700

    Ini adalah Tugas 4 PHP

---
+++ Tugas4php/webmenu.php
@@ -0,0 +1,19 @@
+<?php 
+//array asosiatif
+//menu untuk bagian atas (atas.php)
+$menu_atas = [
+		'home' => 'Home',
+		'produk' => 'Produk',
+		'pesan' => 'Pesan',
+		'galeri' => 'Galeri',
+		'gesbuk' => 'Gesbuk'
+];
+//menu untuk bagian bawah (isi.php)
+$menu_bawah = [
+		'home' => 'home.php',
+		'produk' => 'produk.php',
+		'pesan' => 'pesan.php',
+		'galeri' => 'galeri.php',
+		'gesbuk' => 'gesbuk.php'
+];
+ ?>
